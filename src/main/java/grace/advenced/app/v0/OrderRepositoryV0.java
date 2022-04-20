a22a1912c91cf15bcafe7d1f909cc9e3a80f1fa8
berryberry189 <berryberry189@naver.com>
2022-04-20 23:52:57 +0900

    예제 프로젝트 만들기 V0

---
+++ src/main/java/grace/advenced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,25 @@
+package grace.advenced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId){
+        // 저장 로직
+        if(itemId.equals("ex")){
+            throw new IllegalStateException("예외 발생!!");
+        }
+        sleep(1000); // 1초
+    }
+
+    private void sleep(int mullis){
+        try {
+            Thread.sleep(mullis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
