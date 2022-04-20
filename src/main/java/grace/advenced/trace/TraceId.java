b048bf3c9fbea4a49fccc8bf99c624009c28de9f
berryberry189 <berryberry189@naver.com>
2022-04-21 00:07:49 +0900

    로그 추적기 V1 - TraceId, TraceStatus 생성

---
+++ src/main/java/grace/advenced/trace/TraceId.java
@@ -0,0 +1,45 @@
+package grace.advenced.trace;
+
+import java.util.UUID;
+
+public class TraceId {
+
+    private String id;
+    private int level;
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private TraceId(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    private String createId() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    // id는 같고 깊이만 달라짐
+    public TraceId createNextId(){
+        return new TraceId(id, level + 1);
+    }
+
+    public TraceId createPreviousId(){
+        return new TraceId(id, level - 1);
+    }
+
+    // 첫 레벨인지 확인
+    public boolean isFirstLevel(){
+        return level == 0;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+}
---
+++ src/main/java/grace/advenced/trace/TraceStatus.java
@@ -0,0 +1,27 @@
+package grace.advenced.trace;
+
+public class TraceStatus {
+    // 로그를 시작할때의 상태정보를 가지고 있다. 이 상태 정보는 로그를 종료할 때 사용된다.
+
+    private TraceId traceId;
+    private Long startTimeMs; // 로그 시작 시간
+    private String message; // 시작시 사용 메세지
+
+    public TraceStatus(TraceId traceId, Long startTimeMs, String message) {
+        this.traceId = traceId;
+        this.startTimeMs = startTimeMs;
+        this.message = message;
+    }
+
+    public TraceId getTraceId() {
+        return traceId;
+    }
+
+    public Long getStartTimeMs() {
+        return startTimeMs;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
