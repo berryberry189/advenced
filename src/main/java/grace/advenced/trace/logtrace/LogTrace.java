ced1fa7c68dcaa46291d0e631e2bddc06d7a5077
berryberry189 <berryberry189@naver.com>
2022-05-01 21:58:07 +0900

    필드 동기화 - 개발

---
+++ src/main/java/grace/advenced/trace/logtrace/LogTrace.java
@@ -0,0 +1,13 @@
+package grace.advenced.trace.logtrace;
+
+import grace.advenced.trace.TraceStatus;
+
+// 로그 추척기
+public interface LogTrace {
+
+    TraceStatus begin(String message);
+
+    void end(TraceStatus status);
+
+    void exception(TraceStatus status, Exception e);
+}
