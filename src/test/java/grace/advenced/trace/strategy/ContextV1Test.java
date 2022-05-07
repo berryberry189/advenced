e13d97471813d21572c403e641698fca0f33a63d
berryberry189 <berryberry189@naver.com>
2022-05-07 18:06:25 +0900

    전략 패턴 - 예제2

--- src/test/java/grace/advenced/trace/strategy/ContextV1Test.java
+++ src/test/java/grace/advenced/trace/strategy/ContextV1Test.java
@@ -1,6 +1,7 @@
 package grace.advenced.trace.strategy;
 
 import grace.advenced.trace.strategy.code.ContextV1;
+import grace.advenced.trace.strategy.code.Strategy;
 import grace.advenced.trace.strategy.code.StrategyLogic1;
 import grace.advenced.trace.strategy.code.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
@@ -42,6 +43,66 @@ public class ContextV1Test {
 
         StrategyLogic2 strategyLogic2 = new StrategyLogic2();
         ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        contextV2.execute();
+    }
+
+    /**
+     * 익명 내부 클래스 사용
+     */
+    @Test
+    void strategyV2(){
+        Strategy strategyLogic1 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        };
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        // strategyLogic1=class grace.advenced.trace.strategy.ContextV1Test$1
+        log.info("strategyLogic1={}", strategyLogic1.getClass());
+        contextV1.execute();
+
+        Strategy strategyLogic2 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직2 실행");
+            }
+        };
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        // strategyLogic2=class grace.advenced.trace.strategy.ContextV1Test$2
+        log.info("strategyLogic2={}", strategyLogic2.getClass());
+        contextV2.execute();
+    }
+
+    @Test
+    void strategyV3(){
+        ContextV1 contextV1 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        });
+        contextV1.execute();
+
+        ContextV1 contextV2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직2 실행");
+            }
+        });
+        contextV2.execute();
+    }
+
+    /**
+     * 람다 사용
+     * - 람다로 변경하려면 인터페이스에 메소드가 1개만 있으면 된다.
+     */
+    @Test
+    void strategyV4(){
+        ContextV1 contextV1 = new ContextV1(() -> log.info("비지니스 로직1 실행"));
+        contextV1.execute();
+
+        ContextV1 contextV2 = new ContextV1(() -> log.info("비지니스 로직2 실행"));
         contextV2.execute();
     }
 
--- src/test/java/grace/advenced/trace/strategy/code/StrategyLogic2.java
+++ src/test/java/grace/advenced/trace/strategy/code/StrategyLogic2.java
@@ -6,6 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 public class StrategyLogic2 implements Strategy{
     @Override
     public void call() {
-        log.info("비지니스 로직2 실핼");
+        log.info("비지니스 로직2 실행");
     }
 }
